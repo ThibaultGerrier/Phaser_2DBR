7b905103a3dad8b47a2dc6b04ddd9fdcf68cee06
thibault <thibault.gerrier@student.uibk.ac.at>
2018-03-25 12:59:02 +0200

    fixed webpack, login with react I

--- src/App.js
+++ src/App.js
@@ -1,10 +1,40 @@
 import React from 'react';
+import { Button, FormControl, Grid } from 'react-bootstrap';
 
 export default class App extends React.Component {
+    constructor() {
+        super();
+        this.state = {
+            gameRunning: false,
+            username: '',
+        };
+    }
+
+    play() {
+        this.setState({ gameRunning: true });
+        const { username } = this.state;
+        console.log(username);
+    }
+
     render() {
         return (
             <div>
-                <p>React here!</p>
+                {!this.state.gameRunning &&
+                    <Grid className="center-container">
+                        <h1>Welcome to 2DBR</h1>
+                        <br/>
+                        <FormControl
+                            type="text"
+                            name="emptyLinesInput"
+                            placeholder="Username"
+                            value={this.state.username}
+                            onChange={e => this.setState({ username: e.target.value })}
+                            style={{ width: '50%' }}>
+                        </FormControl>
+                        <br/>
+                        <Button bsStyle="success" onClick={() => this.play()}>Play!</Button>
+                    </Grid>
+                }
             </div>
         );
     }
